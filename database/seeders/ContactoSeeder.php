63b709955520bb5104045be972db39f18b939266
Jean <jeanmarcoalarcon@gmail.com>
2026-03-31 18:06:11 -0400

    Seeders modificados

--- database/seeders/ContactoSeeder.php
+++ database/seeders/ContactoSeeder.php
@@ -12,12 +12,26 @@ class ContactoSeeder extends Seeder
      */
     public function run(): void
     {
-        $jsonPath = base_path('_requisitos/taller2/proyectoMasSimple/personas.json');
+        $jsonPath = database_path('seeders/personas.json');
         if (file_exists($jsonPath)) {
             $json = file_get_contents($jsonPath);
             $personas = json_decode($json, true);
             foreach ($personas as $persona) {
-                \App\Models\Contacto::create($persona);
+                \App\Models\Contacto::create([
+                    'cedula' => $persona['cedula'],
+                    'nombre' => $persona['nombre'],
+                    'apellido' => $persona['apellido'],
+                    'edad' => $persona['edad'],
+                    'genero' => $persona['genero'],
+                    'numero_telefono_1' => $persona['numero_telefono'][0],
+                    'numero_telefono_2' => $persona['numero_telefono'][1] ?? null,
+                    'correo_electronico_1' => $persona['correo_electronico'][0],
+                    'correo_electronico_2' => $persona['correo_electronico'][1] ?? null,
+                    'estado_civil' => $persona['estado_civil'],
+                    'direccion' => $persona['direccion'],
+                    'departamento' => $persona['departamento'],
+                    'cargo' => $persona['cargo'],
+                ]);
             }
         }
     }
